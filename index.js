d28ffbc73f1d362d9a01e4cf3bb984a2f56066a6
Martin Mulyo Syahidin <mulyosyahidin95@gmail.com>
2022-12-23 10:25:29 +0700

    Membuat endpoint untuk mengirim pesan

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
-const qrcode = require('qrcode-terminal');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+const upload = multer();
 
 const client = new Client({
-    authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  var app = express();
+  app.use(cors());
+  app.use(upload.array());
+
+  app.post("/api/send-message", (req, res) => {
+    const { number, message } = req.body;
+
+    client.getNumberId(number).then((contact) => {
+      let formattedNumber = contact._serialized;
+
+      client.sendMessage(formattedNumber, message).then((response) => {
+        let messageId = response.id._serialized;
+
+        res.send({
+          success: true,
+          message: "Message sent successfully",
+          data: {
+            messageId,
+          },
+        });
+      });
+    });
+  });
+
+  app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+  });
 });
 
 client.initialize();
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-
-    //Kirim pesan ke nomor tertentu
-    client.sendMessage('6285709917613@c.us', 'Hello from whatsapp-web.js!');
-    console.log('Pesan berhasil terkirim');
-});
